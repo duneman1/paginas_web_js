dec3eb1352f22f41731d023fa6bd8ae839650e87
duneman1 <duneman.basura@gmail.com>
2018-06-21 13:58:37 +0200

    javascript

---
+++ 01_Datos/hola_funcion.js
@@ -0,0 +1,18 @@
+/*
+    JavaScript (es marca registrada de Oracle) -> ECMAScript (es el standar como W3)
+    ->ES6 / ES2015
+*/
+function saludar(user) {
+    const SALUDO = 'Hola'
+    
+    console.log(SALUDO, user)
+}
+
+function sumar(a,b){
+    let resultado = a + b
+    console.log (resultado)
+}
+saludar('Pepe')
+saludar('Elena')
+
+sumar(2,4)
---
+++ 01_Datos/hola_mundo.js
@@ -0,0 +1,18 @@
+/* Esto es una declaración*/
+var saludo
+
+/* inicializacion */
+saludo = 'Hola'
+/*
+Tipos de datos
+- string (cadena de caracteres)
+- number (2.5)
+- booleans (true / false)
+- object (de ninguno de los anteriores)
+- undefined (sin definir)
+*/
+
+/*Declaración + inicializacion*/
+var user = 'Mundo'
+
+console.log(saludo, user)
